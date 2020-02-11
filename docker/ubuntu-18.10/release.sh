417ecd5eed198ebb756e923e8e222a34ce21298a
Carter Jones <carter@carterjones.info>
2020-02-11 13:29:21 -0800

    replace ubuntu 18.10 with 20.04

--- docker/ubuntu-18.10/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-docker build -f Dockerfile -t carterjones/ubuntu-18.10:latest .
-docker tag carterjones/ubuntu-18.10:latest "carterjones/ubuntu-18.10:${TRAVIS_COMMIT}"
-docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_PASSWORD}"
-docker push "carterjones/ubuntu-18.10:${TRAVIS_COMMIT}"
-docker push "carterjones/ubuntu-18.10:latest"
