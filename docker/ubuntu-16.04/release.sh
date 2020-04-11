f266f10aabdce10bf0c9ec4afea388a9c0c5f698
Carter Jones <carter@carterjones.info>
2020-04-10 23:49:37 -0700

    deprecate ubuntu 14.04 and 16.04 support

--- docker/ubuntu-16.04/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-docker build -f Dockerfile -t carterjones/ubuntu-16.04:latest .
-docker tag carterjones/ubuntu-16.04:latest "carterjones/ubuntu-16.04:${TRAVIS_COMMIT}"
-docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_PASSWORD}"
-docker push "carterjones/ubuntu-16.04:${TRAVIS_COMMIT}"
-docker push "carterjones/ubuntu-16.04:latest"
