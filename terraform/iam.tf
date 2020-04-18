742074b3ed8c57bdb7f4007bc49cba7886234cb2
Carter Jones <carter@carterjones.info>
2020-04-18 15:05:06 -0700

    define admins iam group policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -10,3 +10,33 @@ data "aws_iam_policy_document" "ec2_assume_role" {
     effect = "Allow"
   }
 }
+
+resource "aws_iam_group" "admins" {
+  name = "admins"
+}
+
+resource "aws_iam_policy" "full_access_with_mfa" {
+  name = "full_access_with_mfa"
+  path = "/"
+  description = "Allow all actions when using MFA."
+  policy = data.aws_iam_policy_document.full_access_with_mfa.json
+}
+
+data "aws_iam_policy_document" "full_access_with_mfa" {
+  statement {
+    sid = "FullAccessWithMFA"
+    effect = "Allow"
+    actions = ["*"]
+    resources = ["*"]
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["true"]
+    }
+  }
+}
+
+resource "aws_iam_group_policy_attachment" "admins_full_access_with_mfa" {
+  group      = aws_iam_group.admins.name
+  policy_arn = aws_iam_policy.full_access_with_mfa.arn
+}
