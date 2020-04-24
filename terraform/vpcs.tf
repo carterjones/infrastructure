d26030079c587003496139fbf5b5bd144d98165c
Carter Jones <carter@carterjones.info>
2020-04-24 10:03:31 -0700

    remove us-west-2 vpc

--- terraform/vpcs.tf
+++ terraform/vpcs.tf
@@ -1,9 +1,3 @@
-module "vpc_us_west_1_prod" {
-  source = "./modules/vpc"
-  region = "us-west-1"
-  tier   = "prod"
-}
-
 module "vpc_us_west_2_prod" {
   source = "./modules/vpc"
   region = "us-west-2"
