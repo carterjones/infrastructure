7db21e1f398d5da388747f0d2253612153393bc7
Carter Jones <carter@carterjones.info>
2020-05-05 20:44:59 -0700

    add backup s3 bucket

---
+++ terraform/s3/carterjones-backup.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "carterjones_backup" {
+  bucket        = "carterjones-backup"
+  acl           = "private"
+  force_destroy = false
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = false
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "carterjones_backup" {
+  bucket     = "carterjones-backup"
+  depends_on = [aws_s3_bucket.carterjones_backup]
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
