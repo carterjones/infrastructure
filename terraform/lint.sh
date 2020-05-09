bfcff2c01cc189bfc1aee301a5b5d82a8972aea5
Carter Jones <carter@carterjones.info>
2020-05-08 22:26:17 -0700

    install terraform utilities before running ci

--- terraform/lint.sh
+++ terraform/lint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+set -eux -o pipefail
 
 # Run tflint against all subdirectories.
-curl -L "$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" -o tflint.zip && unzip tflint.zip && rm tflint.zip
-find . -type d | grep -v ".terraform" | xargs -L1 ./tflint
+find . -type d | grep -v ".terraform" | xargs -L1 tflint
