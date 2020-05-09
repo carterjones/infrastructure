4055c1b0402d717e8701da474d1ddb2cca60ff60
Carter Jones <carter@carterjones.info>
2020-05-09 09:35:14 -0700

    run terraform validate in linter

--- terraform/lint.sh
+++ terraform/lint.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 set -eux -o pipefail
 
-# Run tflint against all subdirectories.
-find . -type d | grep -v ".terraform" | xargs -L1 tflint
+# Gather a list of all top-level non-module subdirectories.
+non_module_dirs=()
+while IFS=  read -r -d $'\0'; do
+    non_module_dirs+=("$REPLY")
+done < <(find . -maxdepth 1 -type d -and -not -name "modules" -print0)
+
+# Initialize all top-level non-module subdirectories.
+for dir in "${non_module_dirs[@]}"; do
+    pushd "${dir}" || exit 1
+    if [[ "${CI}" = "true" ]]; then
+        terraform init \
+            -backend-config="bucket=carterjones-terraform-state-ci"
+    else
+        terraform init
+    fi
+    popd
+done
+
+# Run tflint and terraform validate against all top-level non-module
+# subdirectories.
+for dir in "${non_module_dirs[@]}"; do
+    pushd "${dir}" || exit 1
+    tflint
+    terraform validate
+    popd
+done
