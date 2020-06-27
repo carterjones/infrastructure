2c40a0f02cfbb8b83f63c14209a48ad43a6c137a
Carter Jones <carter@carterjones.info>
2020-06-26 17:14:49 -0700

    s/master/main/

--- docker/release.sh
+++ docker/release.sh
@@ -11,7 +11,7 @@ image="${1}"
 docker build -f "Dockerfile.${image}" -t "carterjones/${image}:latest" .
 docker tag "carterjones/${image}:latest" "carterjones/${image}:${TRAVIS_COMMIT}"
 
-if [[ $(git rev-parse --abbrev-ref HEAD) == "master" ]]; then
+if [[ $(git rev-parse --abbrev-ref HEAD) == "main" ]]; then
     docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_PASSWORD}"
     docker push "carterjones/${image}:${TRAVIS_COMMIT}"
     docker push "carterjones/${image}:latest"
