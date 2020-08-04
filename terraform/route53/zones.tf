3fa3a6bd8c1faead6a54cb714b5a44841d63cd36
Carter Jones <carter@carterjones.info>
2020-08-03 20:39:01 -0700

    add carterjones.io zone

--- terraform/route53/zones.tf
+++ terraform/route53/zones.tf
@@ -2,3 +2,8 @@ resource "aws_route53_zone" "carterjones_info" {
   name          = "carterjones.info"
   force_destroy = false
 }
+
+resource "aws_route53_zone" "carterjones_io" {
+  name          = "carterjones.io"
+  force_destroy = false
+}
