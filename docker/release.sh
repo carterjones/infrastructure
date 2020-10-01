903c9cb700b8a4e6aefbc929b67e1b2208898a0c
Carter Jones <carter@carterjones.info>
2020-10-01 11:25:01 -0700

    use travis branch variable for main check

--- docker/release.sh
+++ docker/release.sh
@@ -11,7 +11,7 @@ image="${1}"
 docker build -f "Dockerfile.${image}" -t "carterjones/${image}:latest" .
 docker tag "carterjones/${image}:latest" "carterjones/${image}:${TRAVIS_COMMIT}"
 
-if [[ $(git rev-parse --abbrev-ref HEAD) == "main" ]]; then
+if [[ "${TRAVIS_BRANCH}" == "main" ]]; then
     docker login -u "${DOCKERHUB_USERNAME}" -p "${DOCKERHUB_PASSWORD}"
     docker push "carterjones/${image}:${TRAVIS_COMMIT}"
     docker push "carterjones/${image}:latest"
