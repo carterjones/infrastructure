0baa0a612da6b55bb67bfd82ff0879995a68c5d8
Carter Jones <carter@carterjones.info>
2020-12-13 13:49:07 -0800

    upgrade to terraform 0.13

--- terraform/acm/foundation.tf
+++ terraform/acm/foundation.tf
@@ -8,4 +8,11 @@ terraform {
     bucket = "carterjones-terraform-state-prod"
     region = "us-west-2"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.21.0"
+    }
+  }
 }
--- terraform/s3/foundation.tf
+++ terraform/s3/foundation.tf
@@ -13,6 +13,13 @@ terraform {
     bucket = "carterjones-terraform-state-prod"
     region = "us-west-2"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.21.0"
+    }
+  }
 }
 
 data "aws_caller_identity" "current" {}
