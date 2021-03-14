31925300ec9f0e7ed51f3a00960771136b1ba45e
Carter Jones <carter@carterjones.info>
2021-03-14 10:35:25 -0700

    consolidate cloudfront files

---
+++ terraform/cloudfront/cloudfront.tf
@@ -0,0 +1,34 @@
+module "blog_carterjones_info" {
+  source = "../modules/cloudfront_s3_website"
+  fqdn   = "blog.carterjones.info"
+}
+
+module "carterjones_info" {
+  source = "../modules/cloudfront_s3_website"
+  fqdn   = "carterjones.info"
+}
+
+module "keybase_carterjones_info" {
+  source = "../modules/cloudfront_s3_website"
+  fqdn   = "keybase.carterjones.info"
+}
+
+module "mail_carterjones_info" {
+  source = "../modules/cloudfront_s3_website"
+  fqdn   = "mail.carterjones.info"
+}
+
+module "public_carterjones_info" {
+  source = "../modules/cloudfront_s3_website"
+  fqdn   = "public.carterjones.info"
+}
+
+module "resume_carterjones_info" {
+  source = "../modules/cloudfront_s3_website"
+  fqdn   = "resume.carterjones.info"
+}
+
+module "www_carterjones_info" {
+  source = "../modules/cloudfront_s3_website"
+  fqdn   = "www.carterjones.info"
+}
---
+++ terraform/cloudfront/outputs.tf
@@ -0,0 +1,55 @@
+output "blog_carterjones_info_distribution_domain_name" {
+  value = module.blog_carterjones_info.distribution_domain_name
+}
+
+output "blog_carterjones_info_distribution_hosted_zone_id" {
+  value = module.blog_carterjones_info.distribution_hosted_zone_id
+}
+
+output "carterjones_info_distribution_domain_name" {
+  value = module.carterjones_info.distribution_domain_name
+}
+
+output "carterjones_info_distribution_hosted_zone_id" {
+  value = module.carterjones_info.distribution_hosted_zone_id
+}
+
+output "keybase_carterjones_info_distribution_domain_name" {
+  value = module.keybase_carterjones_info.distribution_domain_name
+}
+
+output "keybase_carterjones_info_distribution_hosted_zone_id" {
+  value = module.keybase_carterjones_info.distribution_hosted_zone_id
+}
+
+output "mail_carterjones_info_distribution_domain_name" {
+  value = module.mail_carterjones_info.distribution_domain_name
+}
+
+output "mail_carterjones_info_distribution_hosted_zone_id" {
+  value = module.mail_carterjones_info.distribution_hosted_zone_id
+}
+
+output "public_carterjones_info_distribution_domain_name" {
+  value = module.public_carterjones_info.distribution_domain_name
+}
+
+output "public_carterjones_info_distribution_hosted_zone_id" {
+  value = module.public_carterjones_info.distribution_hosted_zone_id
+}
+
+output "resume_carterjones_info_distribution_domain_name" {
+  value = module.resume_carterjones_info.distribution_domain_name
+}
+
+output "resume_carterjones_info_distribution_hosted_zone_id" {
+  value = module.resume_carterjones_info.distribution_hosted_zone_id
+}
+
+output "www_carterjones_info_distribution_domain_name" {
+  value = module.www_carterjones_info.distribution_domain_name
+}
+
+output "www_carterjones_info_distribution_hosted_zone_id" {
+  value = module.www_carterjones_info.distribution_hosted_zone_id
+}
