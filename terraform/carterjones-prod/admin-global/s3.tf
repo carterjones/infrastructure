5bb5c29b868b44d366845a0f6fcd9af3e4fc0660
Carter Jones <carter@carterjones.info>
2021-03-14 21:16:45 -0700

    migrate s3 to prod/admin-global

--- terraform/carterjones-prod/admin-global/s3.tf
+++ terraform/carterjones-prod/admin-global/s3.tf
@@ -1,86 +1,86 @@
 # Private buckets.
 
-module "carterjones_terraform_state_ci" {
-  source      = "../modules/s3_bucket"
+module "carterjones_terraform_state_ci_s3" {
+  source      = "../../modules/s3_bucket"
   bucket_name = "carterjones-terraform-state-ci"
 }
 
-module "carterjones_terraform_state_dev" {
-  source      = "../modules/s3_bucket"
+module "carterjones_terraform_state_dev_s3" {
+  source      = "../../modules/s3_bucket"
   bucket_name = "carterjones-terraform-state-dev"
 }
 
-module "carterjones_terraform_state_prod" {
-  source      = "../modules/s3_bucket"
+module "carterjones_terraform_state_prod_s3" {
+  source      = "../../modules/s3_bucket"
   bucket_name = "carterjones-terraform-state-prod"
 }
 
-module "carterjones_info" {
-  source      = "../modules/s3_bucket"
+module "carterjones_info_s3" {
+  source      = "../../modules/s3_bucket"
   bucket_name = "carterjones.info"
   redirect    = "https://www.carterjones.info"
 }
 
-module "git_carterjones_info" {
-  source      = "../modules/s3_bucket"
+module "git_carterjones_info_s3" {
+  source      = "../../modules/s3_bucket"
   bucket_name = "git.carterjones.info"
 }
 
-module "keybase_carterjones_info" {
-  source      = "../modules/s3_bucket"
+module "keybase_carterjones_info_s3" {
+  source      = "../../modules/s3_bucket"
   bucket_name = "keybase.carterjones.info"
   redirect    = "https://carterjones.keybase.pub"
 }
 
-module "mail_carterjones_info" {
-  source      = "../modules/s3_bucket"
+module "mail_carterjones_info_s3" {
+  source      = "../../modules/s3_bucket"
   bucket_name = "mail.carterjones.info"
   redirect    = "https://mail.google.com/a/carterjones.info"
 }
 
-module "res_carterjones_info" {
-  source      = "../modules/s3_bucket"
+module "res_carterjones_info_s3" {
+  source      = "../../modules/s3_bucket"
   bucket_name = "res.carterjones.info"
 }
 
 # Public buckets.
 
-module "blog_carterjones_info" {
-  source              = "../modules/s3_bucket"
+module "blog_carterjones_info_s3" {
+  source              = "../../modules/s3_bucket"
   bucket_name         = "blog.carterjones.info"
   block_public_access = false
   html_page           = "index.html"
 }
 
-module "public_carterjones_info" {
-  source              = "../modules/s3_bucket"
+module "public_carterjones_info_s3" {
+  source              = "../../modules/s3_bucket"
   bucket_name         = "public.carterjones.info"
   block_public_access = false
   html_page           = "index.html"
 }
 
-module "resume_carterjones_info" {
-  source              = "../modules/s3_bucket"
+module "resume_carterjones_info_s3" {
+  source              = "../../modules/s3_bucket"
   bucket_name         = "resume.carterjones.info"
   versioning_enabled  = true
   block_public_access = false
   html_page           = "Carter Jones - Resume.pdf"
 }
 
-module "secure_linux_update" {
-  source              = "../modules/s3_bucket"
+module "secure_linux_update_s3" {
+  source              = "../../modules/s3_bucket"
   bucket_name         = "secure-linux-update"
   block_public_access = false
 }
 
-module "secure_windows_update" {
-  source              = "../modules/s3_bucket"
+module "secure_windows_update_s3" {
+  source              = "../../modules/s3_bucket"
   bucket_name         = "secure-windows-update"
   block_public_access = false
 }
 
-module "www_carterjones_info" {
-  source              = "../modules/s3_bucket"
+module "www_carterjones_info_s3" {
+  source              = "../../modules/s3_bucket"
   bucket_name         = "www.carterjones.info"
   block_public_access = false
   html_page           = "index.html"
