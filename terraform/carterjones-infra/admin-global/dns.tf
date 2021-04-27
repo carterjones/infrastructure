a382ba9d1b63c7c5c3f732f5637153b466d46e2c
Carter Jones <carter@carterjones.info>
2021-04-26 18:10:26 -0700

    disable mx for carterjones.io

--- terraform/carterjones-infra/admin-global/dns.tf
+++ terraform/carterjones-infra/admin-global/dns.tf
@@ -14,21 +14,6 @@ output "carterjones_io_nameservers" {
   value = aws_route53_zone.carterjones_io.name_servers
 }
 
-resource "aws_route53_record" "carterjones_io_mx" {
-  zone_id = aws_route53_zone.carterjones_io.zone_id
-  name    = "carterjones.io"
-  type    = "MX"
-  ttl     = 3600
-
-  records = [
-    "1 ASPMX.L.GOOGLE.COM.",
-    "5 ALT1.ASPMX.L.GOOGLE.COM.",
-    "5 ALT2.ASPMX.L.GOOGLE.COM.",
-    "10 ALT3.ASPMX.L.GOOGLE.COM.",
-    "10 ALT4.ASPMX.L.GOOGLE.COM.",
-  ]
-}
-
 resource "aws_route53_record" "prod_carterjones_info" {
   allow_overwrite = true
   name            = "prod.carterjones.info"
