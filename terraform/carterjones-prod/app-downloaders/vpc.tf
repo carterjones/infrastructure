a97dcb21f8e7404c9e40efa65de0fb2fa09ed991
Carter Jones <carter@carterjones.info>
2021-05-22 09:03:13 -0700

    make a vpc

---
+++ terraform/carterjones-prod/app-downloaders/vpc.tf
@@ -0,0 +1,21 @@
+data "aws_region" "current" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0.0"
+
+  name = "app-downloaders"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${data.aws_region.current.name}a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+  }
+}
