5c42a140298a7035aa765a40043cf3d9d64f6ea5
Carter Jones <carter@carterjones.info>
2021-05-26 22:07:48 -0700

    adjust tfsec settings

--- terraform/carterjones-prod/app-downloaders/security-groups.tf
+++ terraform/carterjones-prod/app-downloaders/security-groups.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "downloaders" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
   }
 
   tags = {
