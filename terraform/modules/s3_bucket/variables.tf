4ededd5accd3833d9d0cad28a9c1c4cf155b7eb1
Carter Jones <carter@carterjones.info>
2022-11-19 14:55:49 -0800

    add types to variables

--- terraform/modules/s3_bucket/variables.tf
+++ terraform/modules/s3_bucket/variables.tf
@@ -1,28 +1,34 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket."
+  type        = string
 }
 
 variable "block_public_access" {
   description = "True if this bucket should be private."
   default     = true
+  type        = bool
 }
 
 variable "versioning_enabled" {
   description = "True if versoning should be enabled."
   default     = false
+  type        = bool
 }
 
 variable "canned_acl" {
   description = "The canned ACL to use for this bucket."
   default     = "private"
+  type        = string
 }
 
 variable "html_page" {
   description = "Optional. A web page to serve from this bucket."
   default     = ""
+  type        = string
 }
 
 variable "redirect" {
   description = "Optional. Make the bucket's static site redirect to this URL."
   default     = ""
+  type        = string
 }
