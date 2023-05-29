27d31cf7594d5321ca359b3636152c9d9a62ce04
Carter Jones <carter@carterjones.info>
2023-05-28 21:45:58 -0700

    increase dns ttls to 48 hours

--- terraform/carterjones-infra/admin-global/dns.tf
+++ terraform/carterjones-infra/admin-global/dns.tf
@@ -33,7 +33,7 @@ output "carterjones_me_nameservers" {
 resource "aws_route53_record" "prod_carterjones_info" {
   allow_overwrite = true
   name            = "prod.carterjones.info"
-  ttl             = 3600
+  ttl             = 172800
   type            = "NS"
   zone_id         = aws_route53_zone.carterjones_info.zone_id
 
@@ -61,7 +61,7 @@ resource "aws_route53_record" "carterjones_info_mx" {
   zone_id = aws_route53_zone.carterjones_info.zone_id
   name    = "carterjones.info"
   type    = "MX"
-  ttl     = 3600
+  ttl     = 172800
 
   records = [
     "10 mail.protonmail.ch.",
@@ -73,7 +73,7 @@ resource "aws_route53_record" "carterjones_info_txt_protonmail" {
   zone_id = aws_route53_zone.carterjones_info.zone_id
   name    = "carterjones.info"
   type    = "TXT"
-  ttl     = 3600
+  ttl     = 172800
   records = [
     "protonmail-verification=899925db5737132194d21148d8cf7d7445bd5275",
     "v=spf1 include:_spf.protonmail.ch mx ~all",
@@ -84,7 +84,7 @@ resource "aws_route53_record" "carterjones_info_dmarc" {
   zone_id = aws_route53_zone.carterjones_info.zone_id
   name    = "_dmarc.carterjones.info"
   type    = "TXT"
-  ttl     = 3600
+  ttl     = 172800
   records = [
     "v=DMARC1; p=reject; pct=100; rua=mailto:ypzqn2df@ag.dmarcian.com;",
   ]
@@ -97,7 +97,7 @@ resource "aws_route53_record" "carterjones_info_dkim1" {
   records = [
     "protonmail.domainkey.ddd22q4jtadwip3fbvp43dv5iokimcahazdp4vk6e3srebrf4qpfq.domains.proton.ch.",
   ]
-  ttl = 3600
+  ttl = 172800
 }
 
 resource "aws_route53_record" "carterjones_info_dkim2" {
@@ -107,7 +107,7 @@ resource "aws_route53_record" "carterjones_info_dkim2" {
   records = [
     "protonmail2.domainkey.ddd22q4jtadwip3fbvp43dv5iokimcahazdp4vk6e3srebrf4qpfq.domains.proton.ch.",
   ]
-  ttl = 3600
+  ttl = 172800
 }
 
 resource "aws_route53_record" "carterjones_info_dkim3" {
@@ -117,7 +117,7 @@ resource "aws_route53_record" "carterjones_info_dkim3" {
   records = [
     "protonmail3.domainkey.ddd22q4jtadwip3fbvp43dv5iokimcahazdp4vk6e3srebrf4qpfq.domains.proton.ch.",
   ]
-  ttl = 3600
+  ttl = 172800
 }
 
 resource "aws_route53_record" "blog_carterjones_info" {
@@ -125,7 +125,7 @@ resource "aws_route53_record" "blog_carterjones_info" {
   name    = "blog.carterjones.info"
   type    = "CNAME"
   records = ["blog.prod.carterjones.info"]
-  ttl     = 3600
+  ttl     = 172800
 }
 
 resource "aws_route53_record" "dm_carterjones_info" {
@@ -133,7 +133,7 @@ resource "aws_route53_record" "dm_carterjones_info" {
   name    = "dm.carterjones.info"
   type    = "CNAME"
   records = ["profile.direct.me"]
-  ttl     = 3600
+  ttl     = 172800
 }
 
 resource "aws_route53_record" "public_carterjones_info" {
@@ -141,7 +141,7 @@ resource "aws_route53_record" "public_carterjones_info" {
   name    = "public.carterjones.info"
   type    = "CNAME"
   records = ["public.prod.carterjones.info"]
-  ttl     = 3600
+  ttl     = 172800
 }
 
 resource "aws_route53_record" "resume_carterjones_info" {
@@ -149,7 +149,7 @@ resource "aws_route53_record" "resume_carterjones_info" {
   name    = "resume.carterjones.info"
   type    = "CNAME"
   records = ["resume.prod.carterjones.info"]
-  ttl     = 3600
+  ttl     = 172800
 }
 
 resource "aws_route53_record" "www_carterjones_info" {
@@ -157,7 +157,7 @@ resource "aws_route53_record" "www_carterjones_info" {
   name    = "www.carterjones.info"
   type    = "CNAME"
   records = ["www.prod.carterjones.info"]
-  ttl     = 3600
+  ttl     = 172800
 }
 
 # ACM validation records.
@@ -167,7 +167,7 @@ resource "aws_route53_record" "blog_carterjones_info_acm" {
   name    = "_6b33d83f44143cbfd7029e9d54394f5a.blog.carterjones.info."
   type    = "CNAME"
   records = ["_b41fba7f7a20923f6283651752d26702.lkwmzfhcjn.acm-validations.aws."]
-  ttl     = 3600
+  ttl     = 172800
 }
 
 resource "aws_route53_record" "carterjones_info_dns_validation" {
@@ -175,7 +175,7 @@ resource "aws_route53_record" "carterjones_info_dns_validation" {
   name    = "_668c8cb47c168219a61e0701b90ff8fa.carterjones.info."
   type    = "CNAME"
   records = ["_543a0c3c0ee64dd49e42ae9d7f3adad6.lkwmzfhcjn.acm-validations.aws."]
-  ttl     = 3600
+  ttl     = 172800
 }
 
 resource "aws_route53_record" "public_carterjones_info_dns_validation" {
@@ -183,7 +183,7 @@ resource "aws_route53_record" "public_carterjones_info_dns_validation" {
   name    = "_199dd41d4fbcec45974ab9d7bb2fb74a.public.carterjones.info."
   type    = "CNAME"
   records = ["_82a8c22d4a1b6acfbb562788d5ae71c9.lkwmzfhcjn.acm-validations.aws."]
-  ttl     = 3600
+  ttl     = 172800
 }
 
 resource "aws_route53_record" "resume_carterjones_info_dns_validation" {
@@ -191,7 +191,7 @@ resource "aws_route53_record" "resume_carterjones_info_dns_validation" {
   name    = "_5b736b9fa9f53b84c4edef29e9558ab8.resume.carterjones.info."
   type    = "CNAME"
   records = ["_6b9767250ee7d1e01c07722bc69f7d49.lkwmzfhcjn.acm-validations.aws."]
-  ttl     = 3600
+  ttl     = 172800
 }
 
 resource "aws_route53_record" "www_carterjones_info_dns_validation" {
@@ -199,7 +199,7 @@ resource "aws_route53_record" "www_carterjones_info_dns_validation" {
   name    = "_3a9cee763740ef90c527fdff762f486b.www.carterjones.info."
   type    = "CNAME"
   records = ["_e0f2f56b3e4ee858e4aa4abd3e93764a.lkwmzfhcjn.acm-validations.aws."]
-  ttl     = 3600
+  ttl     = 172800
 }
 
 #######################
@@ -210,7 +210,7 @@ resource "aws_route53_record" "carterjones_io_txt" {
   zone_id = aws_route53_zone.carterjones_io.zone_id
   name    = "carterjones.io"
   type    = "TXT"
-  ttl     = 3600
+  ttl     = 172800
   records = [
     "sl-verification=fxtnezpppxjqnuslstckmszeqlafgl",
     "v=spf1 include:simplelogin.co ~all",
@@ -221,7 +221,7 @@ resource "aws_route53_record" "carterjones_io_mx" {
   zone_id = aws_route53_zone.carterjones_io.zone_id
   name    = "carterjones.io"
   type    = "MX"
-  ttl     = 3600
+  ttl     = 172800
 
   records = [
     "10 mx1.simplelogin.co.",
@@ -236,7 +236,7 @@ resource "aws_route53_record" "carterjones_io_dkim1" {
   records = [
     "dkim._domainkey.simplelogin.co.",
   ]
-  ttl = 3600
+  ttl = 172800
 }
 
 resource "aws_route53_record" "carterjones_io_dkim2" {
@@ -246,7 +246,7 @@ resource "aws_route53_record" "carterjones_io_dkim2" {
   records = [
     "dkim02._domainkey.simplelogin.co.",
   ]
-  ttl = 3600
+  ttl = 172800
 }
 
 resource "aws_route53_record" "carterjones_io_dkim3" {
@@ -256,14 +256,14 @@ resource "aws_route53_record" "carterjones_io_dkim3" {
   records = [
     "dkim03._domainkey.simplelogin.co.",
   ]
-  ttl = 3600
+  ttl = 172800
 }
 
 resource "aws_route53_record" "carterjones_io_dmarc" {
   zone_id = aws_route53_zone.carterjones_io.zone_id
   name    = "_dmarc.carterjones.io"
   type    = "TXT"
-  ttl     = 3600
+  ttl     = 172800
   records = [
     "v=DMARC1; p=quarantine; pct=100; adkim=s; aspf=s",
   ]
@@ -277,7 +277,7 @@ resource "aws_route53_record" "carterjones_me_txt_protonmail" {
   zone_id = aws_route53_zone.carterjones_me.zone_id
   name    = "carterjones.me"
   type    = "TXT"
-  ttl     = 3600
+  ttl     = 172800
   records = [
     "protonmail-verification=8607a7acf74aa5dc9b5fa41c38b70665fb690312",
   ]
@@ -287,7 +287,7 @@ resource "aws_route53_record" "carterjones_me_mx" {
   zone_id = aws_route53_zone.carterjones_me.zone_id
   name    = "carterjones.me"
   type    = "MX"
-  ttl     = 3600
+  ttl     = 172800
 
   records = [
     "10 mail.protonmail.ch.",
